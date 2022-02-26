2b5bcd3ec81bfa91b150a94617fb7a839761b225
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-02-26 00:15:22 -0500

    updated structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,34 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "1234567890";
 var specialCar = " (!#$%&()*./:;<=>?@[\]^_`{|}~)";
 
+// function to generate password 
+function generatePassword(){
 
+  // ask how many characters they want in the password 
+  var passwordLength = window.prompt("How many characters do you want your password to have? Please choose a length between 8 - 128 characters. ");
+  // convert number of characters 
+  passwordLength = parseInt(passwordLength);
+
+  //confirm if password is correct length 
+  if (passwordLength > 8 && passwordLength < 128) {
+    window.prompt("Please confirm your selection of " + length + " characters.");
+  } else {
+    window.alert("Please provide a valid character amount between 8 and 128.");
+    return;
+  }
+  // ask if they want lower case 
+  var lowerCaseConfirm = window.confirm("Do you want your password to have lower case letters?");
+
+  // ask if they want upper case 
+  var upperCaseConfirm = window.confirm("do you want your password to have upper case letters?");
+
+  // ask if they want special characters 
+  var specialCarConfirm = window.confirm("Do you want special characters in your password?");
+
+  // ask if they want numbers 
+  var numbersConfirm = window.confirm("Do you want numbers in your password?");
+
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -14,31 +41,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-function generatePassword(){
-
-  var passwordLength = window.prompt("How many characters do you want your password to have? Please choose a length between 8 - 128 characters. ");
-  if (passwordLength > 8 && passwordLength < 128) {
-    window.prompt("Please confirm your selection of " + length + " characters.");
-  } else {
-    window.alert("Please provide a valid character amount between 8 and 128.");
-    return;
-  }
-
-  var lowerCaseConfirm = window.confirm("Do you want your password to have lower case letters?");
-
-  var upperCaseConfirm = window.confirm("do you want your password to have upper case letters?");
-
-  var specialCarConfirm = window.confirm("Do you want special characters in your password?");
-
-  var numbersConfirm = window.confirm("Do you want numbers in your password?");
-
-};
-
-generatePassword();
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
